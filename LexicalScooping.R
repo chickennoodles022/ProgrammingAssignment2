73055c3de701a9141f597f4d7574b91f9642232d
chickennoodles022 <91791207+chickennoodles022@users.noreply.github.com>
2021-10-11 13:26:05 +0800

    Update LexicalScooping.R

--- LexicalScooping.R
+++ LexicalScooping.R
@@ -1,31 +1,30 @@
-##this program is created to make a matrix that will also cache the inverse
-makeCacheMatrix <- function(a = matrix()) {
-  I <- NULL
-  set <- function (matrix){ 
-    a <<- matrix
-    I <<- NULL
-  }
-  
-  get <- function() {a} 
-  setInverse <- function (inverse) {I <<- inverse}
-  getinverse <- function() {I}
-  
-  list(set = set, get = get, 
-       setInverse = setInverse, 
-       getInverse = getInverse)
-}
+#ASSIGNMENT FOR WEEK 3 PROG. ASS. 2
+#CREATED by TRISHIA PALOMARES
+#this program is created to cached the inverse of a matrix
 
-#This is for get the inverse from cache
-cacheSolve <- function(x, ...) {
-  a <- x$getInverse()
-  if (!is.null(a)){
-    message ("processing cached information")
-    return(a)
-  }
+#---------------------------------------------------------------------------------------------------------------------------------
+#For the first part, we will create a special matrix that we will use to cached its matrix
+makeCacheMatrix <- function(MTX = matrix()) {
+  reciprocal <- NULL #Y is for inverse because I want to
+  
+  set <- function (matrix){ 
+    MTX <<- matrix #assigning values
+    reciprocal <<- NULL #assigning values }
+  
+  setInverse <- function (inverse) reciprocal <<- inverse
+  get <- function() MTX
+  getinverse <- function() reciprocal
+
+  list(getInverse = getInverse, setInverse = setInverse, get = get, set = set) }}
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#For the second part, we are going to use cacheSolve to compute for the inverse of the marix returned by the first function above.
+cacheSolve <- function(MTX, ...) {
+  reciprocal <- x$getInverse()
+  if (!is.null(reciprocal)){
+    message ("processing cached info") #display to inform them b whatchu doin
+    return(reciprocal) } }
   
   data <- x$get()
-  a <- solve(data) %% data ##solve inverse with mat. multi.
-  x$setInverse(a)
-  a ##return matrix
-}
-
+  reciprocal <- solve(data) %% data ##solve inverse with mat. multi.
+  x$setInverse(reciprocal) reciprocal ##return matrix } %the end adios
